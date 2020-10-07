d0d2815318ac242f2a433df55a5b4f49aaabdc83
Maciej Kowalski <maciej.kowalski@codurance.com>
2020-10-07 13:08:41 +0100

    Initial Structure

---
+++ src/main/java/com/codurance/corporatehotel/CorporatehotelApplication.java
@@ -0,0 +1,13 @@
+package com.codurance.corporatehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporatehotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporatehotelApplication.class, args);
+	}
+
+}
